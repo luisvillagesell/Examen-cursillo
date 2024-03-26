89b42ba07faae724ce68ada1d62efe190d28507d
Luis Gómez <luisvillagesell@gmail.com>
2024-03-26 20:34:14 -0300

    termina el ejercicio

---
+++ scripts.js
@@ -0,0 +1,10 @@
+    let nota = 8;
+
+    if(nota < 4) {
+        console.log("Libre")
+    }  else if (nota >= 4 && nota <7){
+        console.log("Regular")
+    }else{
+        console.log("Promocionado")
+}
+
