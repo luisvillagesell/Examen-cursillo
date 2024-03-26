1c9da1042c27bba14423652032101c92e8915c3a
Luis Gómez <luisvillagesell@gmail.com>
2024-03-26 20:42:28 -0300

    segunda pagina examen

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
-    let nota = 8;
+    let nota = 5;
 
     if(nota < 4) {
         console.log("Libre")
     }  else if (nota >= 4 && nota <7){
         console.log("Regular")
-    }else{
+    } else if (nota >=7 &&  nota <=10) {
         console.log("Promocionado")
+    }else{
+        console.log("Error");
 }
 
