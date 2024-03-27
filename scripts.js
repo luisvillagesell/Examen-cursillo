e99fa2b0671c3cb6000604e8e9dbce60bf62dec3
Luis Gómez <luisvillagesell@gmail.com>
2024-03-27 20:33:31 -0300

    examen cursillo 27mar

--- scripts.js
+++ scripts.js
@@ -1,29 +1,60 @@
-    let nota1 = 0;
-    let nota2 = 0;
-    let nota3 = 0;
-    
-    function calcularMedia(n1, n2, n3) {
-        nota1 = Number(n1);
-        nota2 = Number(n2);
-        nota3 = Number(n3);
-        
-        media = (nota1 + nota2 + nota3)/3;
-        return media.toFixed(2);
-    }
-
-    let nota = calcularMedia(9,7,9);
-    console.log("La nota promedio es : " + nota);
-    function evaluar(nota) {
-    if(nota < 4 && nota >= 1) {
-        return "LIBRE";
-    }  else if (nota >= 4 && nota <7){
-        return "Regular";
-    } else if (nota >=7 &&  nota <=10) {
-        return "Promocionado";
-    }else{
-        return "Error";
+function tipoFigura(lados) {
+    if(lados < 0) {
+        return "Error" + "El numero de lados no es correcto";
+    }  else if (lados == 0) {
+        return "círculo";
+    }   else if(lados == 3) {
+        return "triángulo";
+    }   else if (lados == 4) {
+        return "cuadrado";
+        }   else if (lados == 5) {
+            return "pentagono";
+    }   else if(lados == 6) {
+        return "exágono"
+    }   else {
+        return "polígono con "+ lados + " lados";
     }
 }
 
-let notafinal = evaluar(nota);
-console.log(notafinal)
+let lados = tipoFigura(9);
+console.log(lados);
+
+//esta es la tercera parte de la evalución.
+function perimetroFigura(lados, longitud) {
+    if(lados < 0) {
+        return "Error" + "El numero de lados no es correcto";
+    }
+
+    let figura;
+    let perimetro;
+
+    switch(lados) {
+        case 0:
+            figura = 'círculo';
+            perimetro = 2 * Math.PI * longitud;
+            break;
+        case 3:
+                figura = 'triángulo';
+                perimetro = longitud * 3;
+                break;
+        case 4:
+            figura = 'cuadrado';
+            perimetro = longitud * 4;
+            break;
+        case 5:
+            figura = 'pentagono';
+            perimetro = longitud * 5;
+            break;
+        case 6:
+            figura = 'exágono';
+            perimetro = longitud * 6;
+            break;
+        default:
+            figura = 'polígono con '+ lados + ' lados';
+            perimetro = longitud * lados;
+    }
+    console.log("Tipo de figura geométrica:", figura);
+    console.log("Perímetro:", perimetro);
+}
+
+perimetroFigura(4, 5);
