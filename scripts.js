dc3f5fa103060cee105a56e6415ad0344c03379c
Luis Gómez <luisvillagesell@gmail.com>
2024-03-28 23:14:11 -0300

    final

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
-function tipoFigura(lados) {
-    if(lados < 0) {
-        return "Error" + "El numero de lados no es correcto";
-    }  else if (lados == 0) {
-        return "círculo";
-    }   else if(lados == 3) {
-        return "triángulo";
-    }   else if (lados == 4) {
-        return "cuadrado";
-        }   else if (lados == 5) {
-            return "pentagono";
-    }   else if(lados == 6) {
-        return "exágono"
-    }   else {
-        return "polígono con "+ lados + " lados";
-    }
-}
+// function tipoFigura(lados) {
+//     if(lados < 0) {
+//         return "Error" + "El numero de lados no es correcto";
+//     }  else if (lados == 0) {
+//         return "círculo";
+//     }   else if(lados == 3) {
+//         return "triángulo";
+//     }   else if (lados == 4) {
+//         return "cuadrado";
+//         }   else if (lados == 5) {
+//             return "pentagono";
+//     }   else if(lados == 6) {
+//         return "exágono"
+//     }   else {
+//         return "polígono con "+ lados + " lados";
+//     }
+// }
 
-let lados = tipoFigura(9);
-console.log(lados);
+// let lados = tipoFigura(4);
+// console.log(lados);
 
 //esta es la tercera parte de la evalución.
 function perimetroFigura(lados, longitud) {
@@ -53,8 +53,8 @@ function perimetroFigura(lados, longitud) {
             figura = 'polígono con '+ lados + ' lados';
             perimetro = longitud * lados;
     }
-    console.log("Tipo de figura geométrica:", figura);
-    console.log("Perímetro:", perimetro);
+    console.log("Es un", figura, "y su perímetro es", perimetro);
+    //console.log("Perímetro:", perimetro);
 }
 
 perimetroFigura(4, 5);
